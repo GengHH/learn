7364cbc795918bcdfe96a617533104c52aba0778
geng <18818060415@163.com>
2016-11-16 12:02:16 +0800

    添加注释

--- mockServer/server.js
+++ mockServer/server.js
@@ -3,7 +3,9 @@
  */
 
 
-
+/*
+*使用http模块来构建一个后台服务器
+*/
 var http = require('http');
 
 var content = '▍if you see that,It means you have get the correct data by backend server(mock data by nodejs server)!';
@@ -13,6 +15,10 @@ var srv = http.createServer(function (req, res) {
     res.end(content);
 });
 
+
+/*
+*使用该服务器的时候，对8888端口进行监听
+*/
 srv.listen(8888, function() {
     console.log('listening on localhost:8888');
 });
